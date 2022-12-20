c5c77676d5d0f761623e5dcfa8805739446801ad
Gabbs27 <gabbs27@users.noreply.github.com>
2022-12-20 19:05:00 -0400

    Generador de Facturas REACTJS

---
+++ src/data/countryList.ts
@@ -0,0 +1,6 @@
+const countryList = [
+    { value: 'República Dominicana', text: 'República Dominicana' },
+    { value: 'Estados Unidos', text: 'Estados Unidos' }
+  ]
+  
+  export default countryList
