d5ee9dfbe293593ae47130954bf561e0a2c5d180
Gabriel Abreu <djabreu_@hotmail.com>
2022-12-23 09:07:50 -0400

    Delete src directory

--- src/data/countryList.ts
+++
@@ -1,6 +0,0 @@
-const countryList = [
-    { value: 'República Dominicana', text: 'República Dominicana' },
-    { value: 'Estados Unidos', text: 'Estados Unidos' }
-  ]
-  
-  export default countryList
